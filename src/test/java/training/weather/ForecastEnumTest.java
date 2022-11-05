40f948ace4935dec938c323244f44ab43b2e7175
David Morales <david.dmg89@gmail.com>
2022-11-05 20:19:10 +0100

    add unit test ForecastEnum

---
+++ src/test/java/training/weather/ForecastEnumTest.java
@@ -0,0 +1,21 @@
+package training.weather;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static training.weather.ForecastEnum.PARTLY_CLOUDY;
+
+public class ForecastEnumTest {
+
+    @Test
+    public void getEnumByCode() throws IOException {
+        Assert.assertEquals(PARTLY_CLOUDY, ForecastEnum.getEnumByCode(2));
+    }
+
+    @Test
+    public void getDescription() throws IOException {
+        Assert.assertEquals("Overcast", ForecastEnum.OVERCAST.getDescription());
+    }
+}
