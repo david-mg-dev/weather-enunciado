eb1227fee2fa20eeb5fb5bf3c727fbe2a65404b8
David Morales <david.dmg89@gmail.com>
2022-11-07 11:04:34 +0100

    add & update unit test WeatherForecastTest

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -1,17 +1,30 @@
 package training.weather;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.Date;
-import org.junit.Test;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.*;
 
 public class WeatherForecastTest {
+	WeatherForecast weatherForecast;
 
 	@Test
-	public void unfinished_test() throws IOException {
-		WeatherForecast weatherForecast = new WeatherForecast();
+	public void getCityWeatherTest() throws IOException {
 		String forecast = weatherForecast.getCityWeather("Madrid", new Date());
-		System.out.println(forecast);
+		Assert.assertEquals(ForecastEnum.getEnumByCode(2).getDescription(), "Partly cloudy");
+		Assert.assertFalse(forecast.isEmpty());
+	}
+
+	@Test
+	public void IfDateBefore() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Madrid", new Date(2000-01-01));
+		Assert.assertEquals("", forecast);
+	}
+
+	@Test
+	public void IfDatetimeIsNull() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Madrid", null);
+		Assert.assertFalse(forecast.isEmpty());
 	}
 }
