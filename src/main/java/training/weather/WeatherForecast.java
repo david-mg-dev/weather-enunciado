e487e205281bc33fb74689ee0c821094498cdf4d
David Morales <david.dmg89@gmail.com>
2022-11-07 12:45:26 +0100

    refactor: extract methods & create Class RequestApi

--- src/main/java/training/weather/WeatherForecast.java
+++ src/main/java/training/weather/WeatherForecast.java
@@ -1,12 +1,9 @@
 package training.weather;
 
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -16,26 +13,27 @@ public class WeatherForecast {
 		if (datetime == null) {
 			datetime = new Date();
 		}
+
 		if (datetime.before(new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 6)))) {
-			HttpRequestFactory rf = new NetHttpTransport().createRequestFactory();
-			HttpRequest req = rf
-				.buildGetRequest(new GenericUrl("https://geocode.xyz/" + city + "?json=1"));
-			String r = req.execute().parseAsString();
-			JSONObject object = new JSONObject(r);
-			String longt = object.get("longt").toString();
-			String latt = object.get("latt").toString();
-			rf = new NetHttpTransport().createRequestFactory();
-			req = rf.buildGetRequest(new GenericUrl("https://api.open-meteo.com/v1/forecast?latitude=" +
-                    latt + "&longitude=" + longt + "&daily=weathercode&current_weather=true&timezone=Europe%2FBerlin"));
-			r = req.execute().parseAsString();
-			JSONArray dailyResults = new JSONObject(r).getJSONObject("daily").getJSONArray("time");
-			JSONArray weatherCodeResults = new JSONObject(r).getJSONObject("daily").getJSONArray("weathercode");
+			RequestApi request = new RequestApi();
+
+			String response = request.getResponseGeoCode(city);
+
+			JSONObject objectResponse = new JSONObject(response);
+			String longt = objectResponse.get("longt").toString();
+			String latt = objectResponse.get("latt").toString();
+
+			response = request.getResponseForecast(longt, latt);
+
+			JSONArray dailyResults = new JSONObject(response).getJSONObject("daily").getJSONArray("time");
+			JSONArray weatherCodeResults = new JSONObject(response).getJSONObject("daily").getJSONArray("weathercode");
+
 			for (int i = 0; i < dailyResults.length(); i++) {
 				if (
-				        new SimpleDateFormat("yyyy-MM-dd")
-                                .format(datetime)
-                                .equals(dailyResults.get(i).toString())
-                ) {
+						new SimpleDateFormat("yyyy-MM-dd")
+								.format(datetime)
+								.equals(dailyResults.get(i).toString()))
+				{
 					return ForecastEnum.getEnumByCode((int) weatherCodeResults.get(i)).getDescription();
 				}
 			}
@@ -43,3 +41,5 @@ public class WeatherForecast {
 		return "";
 	}
 }
+
+
